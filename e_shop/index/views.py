91e05ee5874664219ebeead391ab31af827e86f1
farrukhsrdya <frahmatullayev55@gmail.com>
2025-01-28 01:04:49 +0500

    frist

--- e_shop/index/views.py
+++ e_shop/index/views.py
@@ -12,3 +12,24 @@ def home_page(request):
 
     return render(request, 'home.html', context)
 
+
+
+def category_page(request, pk):
+
+    category = Category.objects.get(id=pk)
+
+    news = News.objects.filter(news_category=category)
+
+    context = {'category': category, 'news': news}
+
+    return render(request, 'category.html', context)
+
+
+
+def news_page(request, pk):
+
+    news = News.objects.get(id=pk)
+
+    context = {'news': news}
+
+    return render(request, 'news.html', context)
