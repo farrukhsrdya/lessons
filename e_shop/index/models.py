00d175ee407284c63e93c20eae1007237f39aad2
farrukhsrdya <frahmatullayev55@gmail.com>
2025-01-28 02:34:58 +0500

    first commit

--- e_shop/index/models.py
+++ e_shop/index/models.py
@@ -15,7 +15,7 @@ class News(models.Model):
     news_name = models.CharField(max_length=256)
     news_zogolovok = models.TextField()
     news_osnovnoytext = models.TextField()
-    news_photo = models.ImageField()
+    news_photo = models.ImageField(upload_to='media')
     news_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     added_date = models.DateTimeField(auto_now_add=True)
 
